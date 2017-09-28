02d377af07de10e76e8631813ebdd59dd850cd20
Dmitrii Lezine <dmitriil@microsoft.com>
2017-09-28 17:03:47 -0600

    script

--- ADLab/ADLab/data/installthings.ps1
+++ ADLab/ADLab/data/installthings.ps1
@@ -1,14 +1,12 @@
-﻿# Test Script
+﻿# Create Users in ADDS
 
 Param (
     [string]$user,
     [string]$password
 )
 
+Import-Module ActiveDirectory
 
-# Folders
-New-Item -ItemType Directory c:\FolderCreatedbyScript
-
-
-# Install iis
-Install-WindowsFeature web-server -IncludeManagementTools
+Import-Csv .\ad-data.csv | foreach-object { 
+$userprinicpalname = $_.SamAccountName + "@contoso.com" 
+New-ADUser -SamAccountName $_.SamAccountName -UserPrincipalName $userprinicpalname -Name $_.name -DisplayName $_.name -GivenName $_.cn -SurName $_.sn -Department $_.Department -Path "CN=Users,DC=contoso,DC=com" -AccountPassword (ConvertTo-SecureString "Microsoft~1;" -AsPlainText -force) -Enabled $True -PasswordNeverExpires $True -PassThru }
