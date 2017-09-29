6d30b247cf8f0ebea3b1338f15f3c6df4c97daf7
Dmitrii Lezine <dmitriil@microsoft.com>
2017-09-28 18:40:01 -0600

    fix

--- ADLab/ADLab/data/installthings.ps1
+++ ADLab/ADLab/data/installthings.ps1
@@ -1,12 +1,71 @@
 ﻿# Create Users in ADDS
 
+
+[CmdletBinding()] 
 Param (
     [string]$user,
-    [string]$password
+    [string]$password,
+	[parameter(Mandatory=$true)] 
+    [alias("csv")] 
+    $UsersList, 
+    [parameter(Mandatory=$true)] 
+    [alias("ou")] 
+    $OrganisationalUnit, 
+    [parameter(Mandatory=$true)] 
+    [alias("upn")] 
+    $AdUpn, 
+    [alias("group")] 
+    $AdGroup, 
+    [alias("expire")] 
+    $AdExpire,
+    [switch]$UseSsl
 )
 
 Import-Module ActiveDirectory
+ 
+ 
+## If users list csv file exists then run the script 
+If (Test-Path $userslist) 
+{ 
+    If (Test-Path $UsersList)
 
-Import-Csv .\ad-data.csv | foreach-object { 
-$userprinicpalname = $_.SamAccountName + "@contoso.com" 
-New-ADUser -SamAccountName $_.SamAccountName -UserPrincipalName $userprinicpalname -Name $_.name -DisplayName $_.name -GivenName $_.cn -SurName $_.sn -Department $_.Department -Path "CN=Users,DC=contoso,DC=com" -AccountPassword (ConvertTo-SecureString "Microsoft~1;" -AsPlainText -force) -Enabled $True -PasswordNeverExpires $True -PassThru }
+    $UserCsv = Import-Csv -Path "$UsersList" 
+ 
+    ForEach ($User in $UserCsv) 
+    { 
+        $DisplayName = $User.Firstname + " " + $User.Lastname 
+        $UserFirstName = $User.Firstname 
+        $UserLastName = $User.Lastname 
+        $Sam = $User.SAM 
+        $Upn = $Sam + "@$AdUpn" 
+        $Description = $DisplayName 
+        $Password = $User.Password 
+ 
+        $UserExist = Get-ADUser -filter "SamAccountName -eq '$Sam'" 
+ 
+        If ($UserExist -eq $null) 
+        { 
+            New-ADUser -Name $Sam -DisplayName "$DisplayName" -SamAccountName $Sam -UserPrincipalName $Upn -GivenName "$UserFirstName" -Surname "$UserLastName" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AccountExpirationDate $AdExpire -Verbose 
+         
+            If ($AdGroup) 
+            { 
+                Add-ADGroupMember "$AdGroup" $Sam -Verbose 
+            } 
+        } 
+ 
+        Else 
+        { 
+            Write-Host "User with SAM:$Sam already exists" 
+        } 
+    } 
+} 
+ 
+Else 
+{ 
+    Write-Host "" 
+    Write-Host "There's no user list to work with." 
+    Write-Host "" 
+} 
+ 
+## End
+ 
